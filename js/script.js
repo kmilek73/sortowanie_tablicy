99a01f8075810f87b6fb2ef79caf7bda9019c41a
kmilek <kmilek73@gmail.com>
2022-12-11 19:13:59 +0100

    rozwiązanie

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+let tekst = 'Akademia108';
+
+function tekstToArray (tekst)
+{
+const myArray=tekst.split('');
+console.log(myArray);
+let sortuj = myArray.sort();
+console.log(sortuj);
+}
+
+tekstToArray(tekst);
+
+
+
